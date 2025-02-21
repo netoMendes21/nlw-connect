40d3620ee2dc617e4e7541d148df43991b8d2d7d
José Luiz Medeiros Mendes Neto <jlmendesneto@hotmail.com>
2025-02-21 15:53:47 -0300

    consertando rota de convite

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -13,7 +13,7 @@ interface InvitePageProps {
 export default async function InvitePage(props: InvitePageProps) {
   const { subscriberId } = await props.params;
 
-  const inviteLink = `http://localhost/3333/invites/${subscriberId}`;
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`;
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
